c7727c4dd59b83978100c46d8913030cf429237d
RM-soluciones <itsupport@rmsoluciones.io>
2024-09-20 11:50:38 -0300

    cambio de api key

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,7 @@
 // supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://umvpcsdyfbmgalekufgh.supabase.co'; // Tu URL de Supabase
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtdnBjc2R5ZmJtZ2FsZWt1ZmdoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY0OTMyMTksImV4cCI6MjA0MjA2OTIxOX0.8fGoABy2TKQaqt2ZFjCJ-qmgSgxqIoNbgVjXSK30Tts'; // Tu API Key
+const supabaseUrl = 'https://zynbyczhbsplsyfbuvwu.supabase.co'; // Tu URL de Supabase
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp5bmJ5Y3poYnNwbHN5ZmJ1dnd1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjY1MTQyMDQsImV4cCI6MjA0MjA5MDIwNH0.l_SDvTCaOpu239nxxfUpxsdNERnM5FNCeCjkQ14H7M0'; // Tu API Key
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
